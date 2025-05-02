4553a1872890e4b2776bce50dbac1d3218cbb4e1
Hamza <177459013+hamza-m1@users.noreply.github.com>
2025-05-02 21:30:29 +0100

    Add gameBoard function with a board array

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
-console.log('tic tac to hello')
+function gameBoard() {
+    const rows = 3
+    const columns = 3
+    const board = []
+
+    for (let i = 0; i < rows; i++) {
+        board[i] = []
+        for (let j = 0; j < columns; j++) {
+            board[i].push('test')
+        }
+    }
+
+    return board
+}
+
+gameBoard()
+
+
+
+
+
+
+
+
+
+const testButton = document.querySelector('.test-button')
+
+testButton.addEventListener('click', () => {
+    console.log('hhhhh')
+    gameBoard()
+})
