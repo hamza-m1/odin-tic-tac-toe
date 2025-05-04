05c4c2b0c4e537e3362a800af9cfa5a3b2e91eba
Hamza <177459013+hamza-m1@users.noreply.github.com>
2025-05-04 14:35:25 +0100

    Add prototype of add marker function

--- script.js
+++ script.js
@@ -6,18 +6,33 @@ function gameBoard() {
     for (let i = 0; i < rows; i++) {
         board[i] = []
         for (let j = 0; j < columns; j++) {
-            board[i].push('test')
+            board[i].push(`row:${i}, column:${j}`)
+            //push(cell())
         }
     }
 
     const getBoard = () => board
 
-    return {getBoard}
+    const addMarker = (row, column, player) => {
+        const chosenCell = board[row][column]
+        // console.log(chosenCell)
+
+        if (chosenCell === 0) {
+            console.log('error, choose a free cell')
+        }
+
+        if (chosenCell === 1 || chosenCell === 2) {
+            //markCell(player)
+        }
+        return chosenCell
+    }
+
+    return {getBoard, addMarker}
 }
 
 
-
-
+gameBoard().addMarker(1,2,'harry')
+// console.log(gameBoard().addMarker())
 
 
 
